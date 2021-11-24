7a6f5a4d8adade8da4e8de34a0bd13f0869bc60d
Anton <mblxac@gmail.com>
2021-11-24 16:35:30 +0200

    altered video flow

--- src/components/video.tsx
+++ src/components/video.tsx
@@ -11,57 +11,61 @@ const Video: FC<VideoProps> = props => {
     const socketRef = useRef<Socket | null>(null)
     const myPeerRef = useRef<Peer | null>(null);
     const [userVideos, setUserVideos] = useState<{stream: MediaStream, id: string;}[]>([]);
-    console.log(userVideos)
+
+    const connectStream = async () => {
+        const stream = await navigator.mediaDevices.getUserMedia({
+            video: true,
+            audio: true
+        });
+
+        if (!socketRef.current || !myPeerRef.current) {
+            alert("no sockets")
+            return;
+        }
+
+        setUserVideos([{stream, id: "0"}])
+        myPeerRef.current.on('call', call => {
+            call.answer(stream)
+            call.on('stream', userVideoStream => {
+                setUserVideos(state => ([
+                    ...state.filter(item => item.id !== call.peer),
+                    {stream: userVideoStream, id: call.peer}
+                ]))
+            })
+        })
+
+        socketRef.current.on('user-connected', userId => {
+            if (!myPeerRef.current) {
+                return;
+            }
+            const call = myPeerRef.current.call(userId, stream);
+            call.on('stream', userVideoStream => {
+                setUserVideos(state => ([
+                    ...state.filter(item => item.id !== userId),
+                    {stream: userVideoStream, id: userId}
+                ]));
+            })
+        })
+
+        socketRef.current.on("user-disconnected", userId => {
+            setUserVideos(state => state.filter(video => video.id !== userId))
+        })
+    }
 
     useEffect(() => {
         socketRef.current = io("/", {path: '/api/socketio'});
-
         myPeerRef.current = new Peer();
         // myPeerRef.current = new Peer(undefined, {
         //     host: '/',
         //     port: 3000,
         //     path: '/api/socketio',
         // })
-        navigator.mediaDevices.getUserMedia({
-            video: true,
-            audio: true
-        }).then(stream => {
-            if (!socketRef.current || !myPeerRef.current) {
-                return;
-            }
-
-            setUserVideos([{stream, id: "0"}])
-            myPeerRef.current.on('call', call => {
-                call.answer(stream)
-                call.on('stream', userVideoStream => {
-                    setUserVideos(state => ([
-                        ...state.filter(item => item.id !== call.peer),
-                    {stream: userVideoStream, id: call.peer}
-                    ]))
-                })
-            })
-
-            socketRef.current.on('user-connected', userId => {
-                if (!myPeerRef.current) {
-                    return;
-                }
-                const call = myPeerRef.current.call(userId, stream);
-                call.on('stream', userVideoStream => {
-                    setUserVideos(state => ([
-                        ...state.filter(item => item.id !== userId),
-                        {stream: userVideoStream, id: userId}
-                    ]));
-                })
-            })
-
-            socketRef.current.on("user-disconnected", userId => {
-                setUserVideos(state => state.filter(video => video.id !== userId))
-            })
-        })
 
         myPeerRef.current?.on('open', id => {
             socketRef.current?.emit('join-room', {roomId: props.roomId, userId: id})
         })
+
+        connectStream();
 
     }, [])
     return (
